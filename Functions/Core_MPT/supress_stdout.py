de5d4b24e6e3998a3b538edbbb76461f945f9c0f
JamesElgy <38980526+JamesElgy@users.noreply.github.com>
2023-03-14 14:48:46 +0000

    Small patch to supress_stdout.
    This patch was to account for different interactive python shells, such as jupyter changing the output stream. Change was tested on Windows using Pycharm console, Jupyter notebooks, and terminal.

--- Functions/Core_MPT/supress_stdout.py
+++ Functions/Core_MPT/supress_stdout.py
@@ -10,15 +10,33 @@ def supress_stdout(to=os.devnull):
         print("from Python")
         os.system("echo non-Python applications are also supported")
     """
-    fd = sys.__stdout__.fileno()
+
+    try:
+        if get_ipython().__class__.__name__ == 'ZMQInteractiveShell':
+            fd = sys.__stdout__.fileno()
+        else:
+            fd = sys.stdout.fileno()
+    except:
+        fd = sys.stdout.fileno()
+
 
     ##### assert that Python and C stdio write using the same file descriptor
     ####assert libc.fileno(ctypes.c_void_p.in_dll(libc, "stdout")) == fd == 1
 
     def _redirect_stdout(to):
-        sys.__stdout__.close() # + implicit flush()
-        os.dup2(to.fileno(), fd) # fd writes to 'to' file
-        sys.__stdout__ = os.fdopen(fd, 'w') # Python writes to fd
+        try:
+            if get_ipython().__class__.__name__ == 'ZMQInteractiveShell':
+                sys.__stdout__.close() # + implicit flush()
+                os.dup2(to.fileno(), fd) # fd writes to 'to' file
+                sys.__stdout__ = os.fdopen(fd, 'w') # Python writes to fd
+            else:
+                sys.stdout.close() # + implicit flush()
+                os.dup2(to.fileno(), fd) # fd writes to 'to' file
+                sys.stdout = os.fdopen(fd, 'w') # Python writes to fd
+        except:
+            sys.stdout.close()  # + implicit flush()
+            os.dup2(to.fileno(), fd)  # fd writes to 'to' file
+            sys.stdout = os.fdopen(fd, 'w')  # Python writes to fd
 
     with os.fdopen(os.dup(fd), 'w') as old_stdout:
         with open(to, 'w') as file:
