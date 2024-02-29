7c3ae0a7c81246e0c49579fda769a30f5bfa590c
pdledger <p.d.ledger@keele.ac.uk>
2024-02-29 18:43:23 +0000

    Add files via upload

--- Functions/POD/Constuct_ROM.py
+++ Functions/POD/Constuct_ROM.py
@@ -2,7 +2,7 @@ import numpy as np
 from ngsolve import *
 
 def Construct_ROM(Additional_Int_Order, BigProblem, Mu0, Theta0Sol, alpha, epsi, fes, fes2, inout, mu_inv, sigma,
-                  xivec):
+                  xivec, NumSolverThreads, drop_tol):
     """
     James Elgy - 2023
     Automatically generated function for the construction of the linear and bilinear forms used in the PODP.
@@ -29,31 +29,35 @@ def Construct_ROM(Additional_Int_Order, BigProblem, Mu0, Theta0Sol, alpha, epsi,
     """
 
     print(' creating reduced order model', end='\r')
+    if NumSolverThreads != 'default':
+        SetNumThreads(NumSolverThreads)    
     # Mu0=4*np.pi*10**(-7)
     nu_no_omega = Mu0 * (alpha ** 2)
     Theta_0 = GridFunction(fes)
     u, v = fes2.TnT()
 
     if BigProblem == True:
-        a0 = BilinearForm(fes2, symmetric=True, bonus_intorder=Additional_Int_Order)
+        a0 = BilinearForm(fes2, symmetric=True, bonus_intorder=Additional_Int_Order, delete_zero_elements =drop_tol,keep_internal=False)#, symmetric_storage=True)
     else:
-        a0 = BilinearForm(fes2, symmetric=True, bonus_intorder=Additional_Int_Order)
+        a0 = BilinearForm(fes2, symmetric=True, bonus_intorder=Additional_Int_Order, delete_zero_elements =drop_tol,keep_internal=False)#, symmetric_storage=True)
     a0 += SymbolicBFI((mu_inv) * InnerProduct(curl(u), curl(v)), bonus_intorder=Additional_Int_Order)
     a0 += SymbolicBFI((1j) * (1 - inout) * epsi * InnerProduct(u, v), bonus_intorder=Additional_Int_Order)
 
     if BigProblem == True:
-        a1 = BilinearForm(fes2, symmetric=True)
+        a1 = BilinearForm(fes2, symmetric=True, delete_zero_elements =drop_tol,keep_internal=False)#, symmetric_storage=True)
     else:
-        a1 = BilinearForm(fes2, symmetric=True)
+        a1 = BilinearForm(fes2, symmetric=True, delete_zero_elements =drop_tol,keep_internal=False)#, symmetric_storage=True)
 
     a1 += SymbolicBFI((1j) * inout * nu_no_omega * sigma * InnerProduct(u, v), bonus_intorder=Additional_Int_Order)
-    a0.Assemble()
-    a1.Assemble()
+    with TaskManager():
+        a0.Assemble()
+        a1.Assemble()
     Theta_0.vec.FV().NumPy()[:] = Theta0Sol[:, 0]
     r1 = LinearForm(fes2)
     r1 += SymbolicLFI(inout * (-1j) * nu_no_omega * sigma * InnerProduct(Theta_0, v), bonus_intorder=Additional_Int_Order)
     r1 += SymbolicLFI(inout * (-1j) * nu_no_omega * sigma * InnerProduct(xivec[0], v), bonus_intorder=Additional_Int_Order)
-    r1.Assemble()
+    with TaskManager():
+        r1.Assemble()
 
     read_vec = r1.vec.CreateVector()
     write_vec = r1.vec.CreateVector()
@@ -61,11 +65,13 @@ def Construct_ROM(Additional_Int_Order, BigProblem, Mu0, Theta0Sol, alpha, epsi,
     r2 = LinearForm(fes2)
     r2 += SymbolicLFI(inout * (-1j) * nu_no_omega * sigma * InnerProduct(Theta_0, v), bonus_intorder=Additional_Int_Order)
     r2 += SymbolicLFI(inout * (-1j) * nu_no_omega * sigma * InnerProduct(xivec[1], v), bonus_intorder=Additional_Int_Order)
-    r2.Assemble()
+    with TaskManager():
+        r2.Assemble()
 
     Theta_0.vec.FV().NumPy()[:] = Theta0Sol[:, 2]
     r3 = LinearForm(fes2)
     r3 += SymbolicLFI(inout * (-1j) * nu_no_omega * sigma * InnerProduct(Theta_0, v), bonus_intorder=Additional_Int_Order)
     r3 += SymbolicLFI(inout * (-1j) * nu_no_omega * sigma * InnerProduct(xivec[2], v), bonus_intorder=Additional_Int_Order)
-    r3.Assemble()
+    with TaskManager():
+        r3.Assemble()
     return a0, a1, r1, r2, r3, read_vec, u, v, write_vec
