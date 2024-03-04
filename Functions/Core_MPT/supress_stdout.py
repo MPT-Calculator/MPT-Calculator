a9a65b86f5a4a2a4dd06fb55ef1626dab85a8d14
JamesElgy <38980526+JamesElgy@users.noreply.github.com>
2024-03-04 13:28:05 +0000

    James: Small change to allow print in pytest env

--- Functions/Core_MPT/supress_stdout.py
+++ Functions/Core_MPT/supress_stdout.py
@@ -11,39 +11,49 @@ def supress_stdout(to=os.devnull):
         os.system("echo non-Python applications are also supported")
     """
 
-    try:
-        if get_ipython().__class__.__name__ == 'ZMQInteractiveShell':
-            fd = sys.__stdout__.fileno()
-        else:
-            fd = sys.stdout.fileno()
-    except:
-        fd = sys.stdout.fileno()
-
-
-    ##### assert that Python and C stdio write using the same file descriptor
-    ####assert libc.fileno(ctypes.c_void_p.in_dll(libc, "stdout")) == fd == 1
-
-    def _redirect_stdout(to):
+    # Checks if code is run through pytest session. If so, print statements are handled as normal rather than redirected.
+    # Importantly, this has no effect on the running of the NGSolve finite elements or postprocessing.
+    if "pytest"  not in sys.modules:
+    
         try:
             if get_ipython().__class__.__name__ == 'ZMQInteractiveShell':
-                sys.__stdout__.close() # + implicit flush()
-                os.dup2(to.fileno(), fd) # fd writes to 'to' file
-                sys.__stdout__ = os.fdopen(fd, 'w') # Python writes to fd
+                fd = sys.__stdout__.fileno()
             else:
-                sys.stdout.close() # + implicit flush()
-                os.dup2(to.fileno(), fd) # fd writes to 'to' file
-                sys.stdout = os.fdopen(fd, 'w') # Python writes to fd
+                fd = sys.stdout.fileno()
         except:
-            sys.stdout.close()  # + implicit flush()
-            os.dup2(to.fileno(), fd)  # fd writes to 'to' file
-            sys.stdout = os.fdopen(fd, 'w')  # Python writes to fd
+            fd = sys.stdout.fileno()
 
-    with os.fdopen(os.dup(fd), 'w') as old_stdout:
-        with open(to, 'w') as file:
-            _redirect_stdout(to=file)
+
+        ##### assert that Python and C stdio write using the same file descriptor
+        ####assert libc.fileno(ctypes.c_void_p.in_dll(libc, "stdout")) == fd == 1
+
+        def _redirect_stdout(to):
+            try:
+                if get_ipython().__class__.__name__ == 'ZMQInteractiveShell':
+                    sys.__stdout__.close() # + implicit flush()
+                    os.dup2(to.fileno(), fd) # fd writes to 'to' file
+                    sys.__stdout__ = os.fdopen(fd, 'w') # Python writes to fd
+                else:
+                    sys.stdout.close() # + implicit flush()
+                    os.dup2(to.fileno(), fd) # fd writes to 'to' file
+                    sys.stdout = os.fdopen(fd, 'w') # Python writes to fd
+            except:
+                sys.stdout.close()  # + implicit flush()
+                os.dup2(to.fileno(), fd)  # fd writes to 'to' file
+                sys.stdout = os.fdopen(fd, 'w')  # Python writes to fd
+
+        with os.fdopen(os.dup(fd), 'w') as old_stdout:
+            with open(to, 'w') as file:
+                _redirect_stdout(to=file)
+            try:
+                yield # allow code to be run with the redirected stdout
+            finally:
+                _redirect_stdout(to=old_stdout) # restore stdout.
+                                                # buffering and flags such as
+                                                # CLOEXEC may be different
+
+    else:
         try:
-            yield # allow code to be run with the redirected stdout
+            yield
         finally:
-            _redirect_stdout(to=old_stdout) # restore stdout.
-                                            # buffering and flags such as
-                                            # CLOEXEC may be different
+            pass
