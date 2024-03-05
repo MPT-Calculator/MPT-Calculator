5696cb4a5c4f2e9f054e509bf10ddb47fd1e2589
JamesElgy <38980526+JamesElgy@users.noreply.github.com>
2024-03-05 18:17:14 +0000

    James: moving computing tensors for snaphots outside loop.

--- code_timing.py
+++ code_timing.py
@@ -28,14 +28,14 @@ class LegendTitle(object):
 
 
 
-def measure_memory_usage(n_cpus=2, order=3):
+def measure_memory_usage(n_cpus=1, order=0):
     """
     James Elgy 2024
     Function to run and time the MPT-Calculator main function. This function currently measures the memory usage using the memory_profiler package
     and relies on that to give accurate estimations of the total memory used my the main function.
     
     Options can be configured using the options_dict dictionary.
-    Memory usage and timings are outputted to timings_cpus_{n_cpus} and memory_usage_cpus_{n_cpus} files.
+    Memory usage and timings are outputted to timings_cpus={n_cpus} and memory_usage_cpus={n_cpus} files.
     
     Currently only supports parallel POD.
 
@@ -50,9 +50,10 @@ def measure_memory_usage(n_cpus=2, order=3):
          time_array (Numpy Array): Array of time intervals where memory is queried
          ret (dict): Return dictionary from main function
     """
-    geo = 'OCC_test_sphere_prism_32.py'
-    options_dict = {'order': order, 'geometry': geo, 'use_OCC': True, 'use_POD': True, 'cpus': n_cpus, 'alpha': 0.001,
-                            'use_parallel': True, 'use_iterative_POD': False, 'start_stop':(1,8,81)}
+    
+    geo = 'OCC_test_sphere_prism_32.geo'
+    options_dict = {'order': order, 'geometry': geo, 'use_OCC': False, 'use_POD': True, 'cpus': n_cpus, 'alpha': 0.01,
+                            'use_parallel': True, 'use_iterative_POD': False, 'start_stop':(1,8,40)}
     function_call_time = time()
     mem, ret = memory_usage((main, (), options_dict), interval=0.1, retval=True, include_children=True)
     function_stop_time = time()
@@ -64,9 +65,9 @@ def measure_memory_usage(n_cpus=2, order=3):
     timings['Function_Call_time'] = function_call_time
     timings['Function_Stop_time'] = function_stop_time
 
-    np.save('Results/' + ret['SweepName'] + f'/Data/timings_cpus_{n_cpus}.npy', timings)
+    np.save('Results/' + ret['SweepName'] + f'/Data/Timings_cpus={n_cpus}.npy', timings)
     np.save('Results/' + ret['SweepName'] + f'/Data/ReturnDict.npy', ret)
-    np.save('Results/' + ret['SweepName'] + f'/Data/memory_usage_{n_cpus}.npy', [mem, time_array])
+    np.save('Results/' + ret['SweepName'] + f'/Data/memory_usage_cpus={n_cpus}.npy', [mem, time_array])
 
     return mem, timings, function_call_time, time_array, ret
 
@@ -80,23 +81,28 @@ def plot_times(Datadir: str, n_cpus: int):
         n_cpus (int): number of cpus used in the sweep of interest.
     """
     
-    
+    plt.figure()
     cols = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:cyan', 'tab:pink']
 
-    times = np.load(Datadir + f'timings_cpus_{n_cpus}.npy', allow_pickle=True).all()
-    mem = np.load(Datadir + f'memory_usage_{n_cpus}.npy', allow_pickle=True)
-
+    times = np.load(Datadir + f'Timings_cpus={n_cpus}.npy', allow_pickle=True).all()
+    
+    try:
+        mem = np.load(Datadir + f'memory_usage_cpus={n_cpus}.npy', allow_pickle=True)
+        mem_array_mat = mem[0,:]
+        time_array_mat = mem[1,:]
+        plt.plot(time_array_mat, mem_array_mat, label='Memory Usage', color='k')
+    except FileNotFoundError:
+        pass
     
     # Doing Plot
-    plt.figure()
     timings = times
-    function_call_time = timings['Function_Call_time']
-
-    mem_array_mat = mem[0,:]
-    time_array_mat = mem[1,:]
-
-    plt.plot(time_array_mat, mem_array_mat, label='Memory Usage', color='k')
-
+    
+    try:
+        function_call_time = timings['Function_Call_time']
+    except KeyError:
+        print(f'The call time for the main function was not recorded. An estimate for the total time is provided by {timings["Tensors"] - timings["start_time"] = }s')
+        function_call_time = timings['start_time']
+        
     timing_list = [timings['start_time'] - function_call_time,
                     timings['Theta0'] - function_call_time,
                     timings['Theta1'] - function_call_time,
@@ -129,9 +135,11 @@ def plot_times(Datadir: str, n_cpus: int):
 
 
 if __name__ == '__main__':
-    mem, timings, function_call_time, time_array, ret = measure_memory_usage(n_cpus=4, order=0)
+    mem, timings, function_call_time, time_array, ret = measure_memory_usage(n_cpus=1, order=0)
     Datadir = 'Results/' + ret['SweepName'] + '/Data/'
-    plot_times(Datadir, 4)
+    
+    # Datadir = r'/home/james/Desktop/MPT-Calculator-Testing-Branch/MPT-Calculator/Results/OCC_Gun_modelv2_30/al_0.01_mu_1,100_sig_3.5e7,4.5e6/1e1-1e8_40_el_32925_ord_5_POD_13_1e-6/Data/'
+    plot_times(Datadir, 1)
     plt.show()
 
 
