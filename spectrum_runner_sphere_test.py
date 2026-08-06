25f178865e0bec9746bec4bc611f926026d71544
Paul Ledger <pdl11@mcs.le.ac.uk>
2026-08-06 16:44:04 +0100

    Paul update 6th August 2026

--- spectrum_runner_sphere_test.py
+++ spectrum_runner_sphere_test.py
@@ -12,10 +12,10 @@ if __name__ == '__main__':
    PODArray=np.array([1e-3, 7892.91669004,  25374.73864028,  53653.53300996,  93022.35270049,
        143562.46349437, 205300.95530173, 278248.54603154, 362410.08502918,
        457788.00535374, 564383.63050264, 682197.72835656, 1e12])
-   for order in [3]:
+   for order in [5]:
        print("solving order=",order)
        #Return_Dict = main(geometry=geometry,use_POD=False,use_parallel=False,use_OCC=True,start_stop=(-3,12,500), MPT_Eigen=False,curve_degree=5,N_POD_points=13,MPT_Eigen_From_POD=True, order=order,cpus=CPUs[order],Amp_scale=2*np.pi*0.01**3,  Time=np.logspace(-6,-1,300))
-       for N in [500,1000,3000]:
-           Return_Dict = main(geometry=geometry,use_POD=True, use_parallel=False, use_OCC=True, start_stop=(-3,12,N), MPT_Eigen=False,curve_degree=5,N_POD_points=13,MPT_Eigen_From_POD=True, SingleSVD=False, order=order, PODArray='default',cpus=CPUs[order], Amp_scale=2*np.pi*0.01**3,  Time=np.logspace(-6,-1,300))
+       for N in [100]:
+           Return_Dict = main(geometry=geometry,use_POD=True, use_parallel=False, use_OCC=True, start_stop=(-3,12,N), MPT_Eigen=False,curve_degree=5,N_POD_points=50,MPT_Eigen_From_POD=True, SingleSVD=False, order=order, PODArray='default',cpus=CPUs[order], Amp_scale=2*np.pi*0.01**3,  Time=np.logspace(-6,-1,300))
 
    stop_time = time();
